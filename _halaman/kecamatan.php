6ed8a7d9affef57bf796702c9230305c4c7acf27
as-shiddiq <n.shiddiq@gmail.com>
2019-12-03 12:14:22 +0800

    Upload GeOJSON

--- _halaman/kecamatan.php
+++ _halaman/kecamatan.php
@@ -3,46 +3,64 @@
   $judul=$title;
   $url='kecamatan';
 if(isset($_POST['simpan'])){
+	$file=upload('geojson_kecamatan','geojson');
+	if($file!=false){
+		$data['geojson_kecamatan']=$file;
+	}
 	if($_POST['id_kecamatan']==""){
 		$data['kd_kecamatan']=$_POST['kd_kecamatan'];
 		$data['nm_kecamatan']=$_POST['nm_kecamatan'];
-		$db->insert("m_kecamatan",$data);
-		?>
-		<script type="text/javascript">
-			window.alert('sukses disimpan');
-			window.location.href="<?=url('kecamatan')?>";
-		</script>
-		<?php
+		$exec=$db->insert("m_kecamatan",$data);
+		$info='<div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Sukses!</h4> Data Sukses Ditambah </div>';
+		
 	}
 	else{
 		$data['kd_kecamatan']=$_POST['kd_kecamatan'];
 		$data['nm_kecamatan']=$_POST['nm_kecamatan'];
 		$db->where('id_kecamatan',$_POST['id_kecamatan']);
-		$db->update("m_kecamatan",$data);
-		?>
-		<script type="text/javascript">
-			window.alert('sukses diubah');
-			window.location.href="<?=url('kecamatan')?>";
-		</script>
-		<?php
+		$exec=$db->update("m_kecamatan",$data);
+		$info='<div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Sukses!</h4> Data Sukses diubah </div>';
 	}
+
+	if($exec){
+		$session->set('info',$info);
+	}
+	else{
+      $session->set("info",'<div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Error!</h4> Proses gagal dilakukan
+              </div>');
+	}
+	redirect(url($url));
 }
 
 if(isset($_GET['hapus'])){
 	$db->where("id_kecamatan",$_GET['id']);
-	$db->delete("m_kecamatan");
-	?>
-	<script type="text/javascript">
-		window.alert('sukses dihapus');
-		window.location.href="<?=url('kecamatan')?>";
-	</script>
-	<?php
+	$exec=$db->delete("m_kecamatan");
+	$info='<div class="alert alert-success alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <h4><i class="icon fa fa-ban"></i> Sukses!</h4> Data Sukses dihapus </div>';
+	if($exec){
+		$session->set('info',$info);
+	}
+	else{
+      $session->set("info",'<div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Error!</h4> Proses gagal dilakukan
+              </div>');
+	}
+	redirect(url($url));
 }
 
 if(isset($_GET['tambah']) OR isset($_GET['ubah'])){
   $id_kecamatan="";
   $kd_kecamatan="";
   $nm_kecamatan="";
+  $geojson_kecamatan="";
   if(isset($_GET['ubah']) AND isset($_GET['id'])){
   	$id=$_GET['id'];
   	$db->where('id_kecamatan',$id);
@@ -51,11 +69,12 @@ if(isset($_GET['tambah']) OR isset($_GET['ubah'])){
 		$id_kecamatan=$row->id_kecamatan;
 		$kd_kecamatan=$row->kd_kecamatan;
 		$nm_kecamatan=$row->nm_kecamatan;
+		$geojson_kecamatan=$row->geojson_kecamatan;
 	}
   }
 ?>
 <?=content_open('Form Kecamatan')?>
-    <form method="post">
+    <form method="post" enctype="multipart/form-data">
     	<?=input_hidden('id_kecamatan',$id_kecamatan)?>
     	<div class="form-group">
     		<label>Kode Kecamatan</label>
@@ -64,6 +83,10 @@ if(isset($_GET['tambah']) OR isset($_GET['ubah'])){
     	<div class="form-group">
     		<label>Nama Kecamatan</label>
     		<?=input_text('nm_kecamatan',$nm_kecamatan)?>
+    	</div>
+    	<div class="form-group">
+    		<label>GeoJSON</label>
+    		<?=input_file('geojson_kecamatan',$geojson_kecamatan)?>
     	</div>
     	<div class="form-group">
     		<button type="submit" name="simpan" class="btn btn-info"><i class="fa fa-save"></i> Simpan</button>
@@ -77,12 +100,15 @@ if(isset($_GET['tambah']) OR isset($_GET['ubah'])){
 
 <a href="<?=url($url.'&tambah')?>" class="btn btn-success" ><i class="fa fa-plus"></i> Tambah</a>
 <hr>
+<?=$session->pull("info")?>
+
 <table class="table table-bordered">
 	<thead>
 		<tr>
 			<th>No</th>
 			<th>Kode</th>
 			<th>Nama Kecamatan</th>
+			<th>GeoJSON</th>
 			<th>Aksi</th>
 		</tr>
 	</thead>
@@ -96,6 +122,7 @@ if(isset($_GET['tambah']) OR isset($_GET['ubah'])){
 						<td><?=$no?></td>
 						<td><?=$row->kd_kecamatan?></td>
 						<td><?=$row->nm_kecamatan?></td>
+						<td><a href="<?=assets('unggah/geojson/'.$row->geojson_kecamatan)?>" target="_BLANK"><?=$row->geojson_kecamatan?></a></td>
 						<td>
 							<a href="<?=url($url.'&ubah&id='.$row->id_kecamatan)?>" class="btn btn-info"><i class="fa fa-edit"></i> Ubah</a>
 							<a href="<?=url($url.'&hapus&id='.$row->id_kecamatan)?>" class="btn btn-danger" onclick="return confirm('Hapus data?')"><i class="fa fa-trash"></i> Hapus</a>
